c2a9736c793d638d4317ba318d8d5418abf6b009
ACCIONLABS\Al1430 <pradeepjack555@gmail.com>
2020-03-15 11:38:18 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Clone this repository
+git clone https://github.com/PradeepDevK/Mass_Booking.git
+# Go into the repository
+cd Mass_Booking
+# Install dependencies
+npm install
+# Run the app
+npm start
---
+++ main.js
@@ -0,0 +1,109 @@
+const {app, BrowserWindow, Menu, ipcMain} = require('electron')
+
+var path = require('path')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+
+function createWindow () {
+    // Create the browser window.
+    win = new BrowserWindow({
+        width: 1366,
+        height: 768,
+        icon: path.join(__dirname, 'assets/icons/png/64x64.png'),
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    // and load the index.html of the app.
+    win.loadFile('index.html')
+
+    // Open the DevTools.
+    win.webContents.openDevTools()
+
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null
+    })
+
+    //Build menu from template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    //Insert Menu
+    Menu.setApplicationMenu(mainMenu);
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow()
+    }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+//Create menu template
+const mainMenuTemplate = [{
+    label: 'File',
+    submenu: [
+        {
+            label: 'Quit',
+            accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+            click() {
+                app.quit();
+            }
+        }
+    ]
+}//, 
+// {
+//     label: 'Report',
+//     submenu: [
+//         {
+//             label: 'View',
+//             click() {
+//                 win.loadFile('view.html')
+//             }
+//         }
+//     ]
+// }
+];
+
+//If mac, add empty object to menu
+if (process.platform == 'drawin') {
+    mainMenuTemplate.unshift({});
+}
+
+//Add developer tools item if not in prod
+if (process.env.NODE_ENV !== 'production') {
+    mainMenuTemplate.push({
+        label: 'Developer Tools',
+        submenu: [{
+            label: 'Toogle DevTools',
+            accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+            click(item, focusedWindow) {
+                focusedWindow.toggleDevTools();
+            }
+        }, {
+            role: 'reload'
+        }]
+    });
+}
